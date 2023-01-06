30d92bfc3cfdd49fb6b563fa77c4f4e2b86af7d6
Akash kumar <akash8825317696@gmail.com>
2023-01-06 23:20:04 +0530

    commit 2
    modified styling and some functionalities

--- main.js
+++ main.js
@@ -1,45 +1,10 @@
-// //variables
-
-// const addTask = document.querySelector('#add-task')
-// const taskContainer = document.querySelector('#task-container')
-// const  inputTask = document.querySelector('#input-task')
-
-// //Event Listener
-// addTask.addEventListener('click',function(){
-
-//     let task = document.createElement('div');
-//     task.classList.add('task');
-
-//     let li =document.createElement('li');
-//     li.innerText = `${inputTask.value}`;
-//     task.appendChild(li);
-
-//     let checkButton = document.createElement("button")
-//     checkButton.innerHTML = `<i class="fa fa-check-square-o"></i>`
-//     checkButton.classList.add('checkTask')
-//     task.appendChild(checkButton)
-
-//     let deleteButton = document.createElement("button")
-//     deleteButton.innerHTML = `<i class="fa fa-trash"></i>`
-//     deleteButton.classList.add('deleteTask')
-//     task.appendChild(deleteButton)
-
-//     if(inputTask.value === ""){
-//         alert('Please Enter a Task!')
-//     }else 
-//     taskContainer.appendChild(task)
-
-// })
-
-
-
-//pratice
-
 //variables
 
 const addTask = document.querySelector('#add-task')
 const inputTask = document.querySelector('#input-task')
 const taskContainer = document.querySelector('#task-container')
+
+//Event Listener
 
 addTask.addEventListener('click',function(){
 
@@ -63,7 +28,28 @@ addTask.addEventListener('click',function(){
 
     if(inputTask.value === ""){
         alert('Please Enter a Task')
-    }else
+    }else{
         taskContainer.appendChild(task)
+    }
+
+    inputTask.value = ""
+
+    checkButton.addEventListener('click',function(){
+
+        // console.log(checkButton.parentElement)
+        checkButton.parentElement.style.textDecoration = "line-through"
+
+    })
+
+    deleteButton.addEventListener('click',function(e){
+
+        let target = e.target
+
+        target.parentElement.parentElement.remove()
+
+        // console.log(deleteButton.parentElement)
+        // deleteButton.parentElement.remove('div')
+
+    })
 
 })
