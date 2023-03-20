08d52358222017a8010d9b96db68c26fc2de946a
Gaurav10997 <avdhika@gmail.com>
2023-03-21 02:16:14 +0530

    added a local storage

--- main.js
+++ main.js
@@ -5,6 +5,64 @@ const inputTask = document.querySelector('#input-task')
 const taskContainer = document.querySelector('#task-container')
 
 //Event Listener
+var items = []
+if(JSON.parse(localStorage.getItem("items"))==null){
+    localStorage.setItem("items",JSON.stringify("d"))
+}
+
+const local = JSON.parse(localStorage.getItem("items"));
+ items = [...local]
+function creating(i , items){
+    let task = document.createElement('div')
+    task.classList.add('task')
+
+    let li = document.createElement('li')
+    li.innerText = `${items[i]}`
+    li.classList.add('list')
+    task.appendChild(li)
+
+    let checkButton = document.createElement('button')
+    checkButton.classList.add('checkTask')
+    checkButton.innerHTML = `<i class="fa fa-check-square-o" id="checkBox"></i>`
+    task.appendChild(checkButton)
+
+    let deleteButton = document.createElement('button')
+    deleteButton.classList.add('deleteTask')
+    deleteButton.innerHTML = `<i class="fa fa-trash" id="trash"></i>`
+    task.appendChild(deleteButton)
+    taskContainer.appendChild(task)
+
+    checkButton.addEventListener('click',function(){
+
+        // console.log(checkButton.parentElement)
+        checkButton.parentElement.style.textDecoration = "line-through"
+
+    })
+
+    deleteButton.addEventListener('click',function(){
+
+        // let target = e.target
+
+        // target.parentElement.parentElement.remove('div')
+
+        // console.log(deleteButton.parentElement)
+        deleteButton.parentElement.remove('div')
+
+    })
+}
+
+// creating(0,items);
+// creating(1,items);
+// creating(2,items);
+
+for(let i = 1 ; i< items.length ; i++){
+    creating(i,items);
+
+}
+
+
+
+
 
 addTask.addEventListener('click',function(){
 
@@ -12,6 +70,8 @@ addTask.addEventListener('click',function(){
     task.classList.add('task')
 
     let li = document.createElement('li')
+    items.push(inputTask.value)
+    localStorage.setItem("items",JSON.stringify(items))
     li.innerText = `${inputTask.value}`
     li.classList.add('list')
     task.appendChild(li)
@@ -49,7 +109,7 @@ addTask.addEventListener('click',function(){
 
         // console.log(deleteButton.parentElement)
         deleteButton.parentElement.remove('div')
-
     })
 
 })
+
